aa10934bb649e0f8ae5446257e8759ab56864ea9
fisty256 <fisty256@gmail.com>
2015-02-08 18:02:23 +0100

    New blocks, items, start of GUIs

---
+++ src/main/java/net/fisty256/affs/item/ItemLinkCard.java
@@ -0,0 +1,7 @@
+package net.fisty256.affs.item;
+
+import net.minecraft.item.Item;
+
+public class ItemLinkCard extends Item {
+	
+}
---
+++ src/main/java/net/fisty256/affs/proxy/CommonProxy.java
@@ -0,0 +1,8 @@
+package net.fisty256.affs.proxy;
+
+public class CommonProxy {
+	public void registerRenders()
+	{
+		
+	}
+}
