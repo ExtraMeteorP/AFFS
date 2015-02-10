7df81643d85e62af7515cfdf80d20a95a1b2ebf1
fisty256 <fisty256@gmail.com>
2015-02-10 17:38:13 +0100

    Server startup crash fixed (stupid me..)

--- src/main/java/net/fisty256/affs/reference/ProxyReferences.java
+++ src/main/java/net/fisty256/affs/reference/ProxyReferences.java
@@ -3,5 +3,5 @@ package net.fisty256.affs.reference;
 public class ProxyReferences {
 	public static final String
 		CLIENT_PROXY_CLASS = "net.fisty256.affs.proxy.ClientProxy",
-		SERVER_PROXY_CLASS = "net.fisty256.affs.proxy.ServerProxy";
+		SERVER_PROXY_CLASS = "net.fisty256.affs.proxy.CommonProxy";
 }
