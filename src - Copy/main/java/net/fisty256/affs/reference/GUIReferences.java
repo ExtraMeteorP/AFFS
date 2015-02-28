0436d242c9f2bc61543082740f728548e2179d5f
fisty256 <fisty256@gmail.com>
2015-02-28 17:11:53 +0100

    Removed doubled src folder

--- src - Copy/main/java/net/fisty256/affs/reference/GUIReferences.java
+++
@@ -1,9 +0,0 @@
-package net.fisty256.affs.reference;
-
-public class GUIReferences {
-	public static final int
-		ID_FORCE_GENERATOR = 0,
-		ID_FORCE_FIELD_PROJECTOR = 1,
-		ID_FORCE_CHARGER = 2,
-		ID_CONFIGURATOR = 3;
-}
--- src - Copy/main/java/net/fisty256/affs/reference/ModReferences.java
+++
@@ -1,7 +0,0 @@
-package net.fisty256.affs.reference;
-
-public class ModReferences {
-	public static final String
-		VERSION = "dev1.0.0",
-		MODID = "affs";
-}
